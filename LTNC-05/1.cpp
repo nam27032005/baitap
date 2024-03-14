5a341a88844ad6ea63a61ddf094e8f6e5ef2888f
nam27032005 <160881524+nam27032005@users.noreply.github.com>
2024-03-14 20:08:39 +0700

    Update 1.cpp

--- LTNC-05/1.cpp
+++ LTNC-05/1.cpp
@@ -1,1 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+void update(int *a, int *b) {
+    cout << *a + *b << endl;
+    cout << abs(*a - *b);
+}
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    int *pa=&a, *pb=&b;
+    update(pa,pb);
+}
